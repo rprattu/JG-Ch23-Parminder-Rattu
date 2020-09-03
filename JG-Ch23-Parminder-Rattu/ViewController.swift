6edeae88e5b03b1fb4bcad063ca9d931bc7da340
Parminder Rattu <prattu@mail.sfsu.edu>
2020-09-03 12:52:18 -0700

    you are awesome

---
+++ JG-Ch23-Parminder-Rattu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JG-Ch23-Parminder-Rattu
+//
+//  Created by Priya Rattu on 9/3/20.
+//  Copyright © 2020 SFSU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
